71f5426a510f4f738be0c9fde7a4971fec997894
Jose Rodrigues <joserodriguesrsi18@gmail.com>
2023-05-29 23:48:54 +0100

    Connection do DB

---
+++ postgresqlConnection.py
@@ -0,0 +1,48 @@
+"""
+INFO about postgresql:
+https://www.cherryservers.com/blog/how-to-install-and-setup-postgresql-server-on-ubuntu-20-04
+
+"""
+import psycopg2, os, ssl
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv('environmentVariables.env')
+
+
+# Retrieve the value of the environment variable
+password = os.environ.get('POSTGRESQL_PASSWORD')
+
+conn = psycopg2.connect(
+    host="192.168.1.12",
+    port=5432,
+    database="links",
+    user="postgres",
+    password=password,
+)
+
+cursor = conn.cursor()
+
+#create_table_query = """
+#CREATE TABLE urlInfo (
+#    id serial PRIMARY KEY,
+#    urlName VARCHAR
+#)
+#"""
+
+#cursor.execute(create_table_query)
+
+
+insert_query = "INSERT INTO urlInfo (urlName) VALUES (%s)"
+data = [("https://totalsportek.pro/")]  # Example data
+
+cursor.execute(insert_query, data)
+
+#Commit the transaction to make the changes permanent
+conn.commit()
+
+#After executing the queries, close the cursor and the database connection
+cursor.close()
+conn.close()
+
+
