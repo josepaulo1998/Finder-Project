016ed97cd71a4ec01545b2a910d8777a4a5c32a5
Jose Rodrigues <joserodriguesrsi18@gmail.com>
2023-05-29 13:07:34 +0100

    Branch for External Crawling

--- webscraping.py
+++ webscraping.py
@@ -7,7 +7,7 @@ import requests
 def httpRequest():
   
     #Extracts all the HTML page
-    """
+    
     #PoolManager instance for sending requests
     http = urllib3.PoolManager()
 
@@ -15,8 +15,8 @@ def httpRequest():
     resp = http.request("GET","https://totalsportek.pro/", preload_content=False)
     resp.auto_close = False
 
-    myfile = open('sportek.html','w')
+    myfile = open('C:/Trabalho/Projects/Walktrough/Finder/sportek.html','w')
     for line in io.TextIOWrapper(resp):
         myfile.write(line)
     myfile.close()
-    """
+    
